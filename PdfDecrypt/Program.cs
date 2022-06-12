81dac332d39a61328995e1a0e46944d22cb8f1df
34j <55338215+34j@users.noreply.github.com>
2022-06-12 16:47:05 +0900

    Initial commit

---
+++ PdfDecrypt/Program.cs
@@ -0,0 +1,109 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+using PdfSharpCore.Pdf.Security;
+using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.IO;
+
+namespace PdfDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Argument(0, Description = "The path of the file to remove the password from."), Required]
+        public string FromPath { get; private set; } = "";
+        [Argument(1, Description = "The path to save the decrypted file to. (Optional)")]
+        public string ToPath { get; private set; } = "";
+        [Option(ShortName = "p", LongName = "password", Description = "The password to use to decrypt the file.")]
+        public string Password { get; private set; } = "";
+        [Option(ShortName = "nc", LongName = "noclose", Description = "Whether not to close the application after decrypting the file.", ShowInHelpText = false)]
+        public bool NoClose { get; private set; } = false;
+        [Option(ShortName = "a", LongName = "addextension", Description = "Whether to add \".pdf\" to FromPath or ToPath if it does not have extension.", ShowInHelpText = false)]
+        public bool AddExtension { get; private set; } = false;
+        private void OnExecute()
+        {
+            try
+            {
+                Execute();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"{e.GetType()}: {e.Message}");
+            }
+            if (NoClose)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private void Execute()
+        {
+            // Check FromPath
+            FromPath = Path.GetFullPath(FromPath ?? "");            
+            if (!Path.HasExtension(FromPath) && AddExtension)
+            {
+                FromPath = Path.ChangeExtension(FromPath, ".pdf");
+            }
+            if (!File.Exists(FromPath))
+            {
+                throw new FileNotFoundException("The file to decrypt does not exist.", FromPath);
+            }
+
+            // Check ToPath
+            if (string.IsNullOrEmpty(ToPath))
+            {
+                ToPath = Path.ChangeExtension(FromPath, ".decrypted.pdf");
+            }
+            else
+            {
+                ToPath = Path.GetFullPath(ToPath);
+                if (!Path.HasExtension(ToPath) && AddExtension)
+                {
+                    ToPath = Path.ChangeExtension(ToPath, ".pdf");
+                }
+            }
+
+            // Check Password
+            if (string.IsNullOrEmpty(Password))
+            {
+                Password = Prompt.GetPassword("Password: ");
+            }
+
+            // Execute
+            RemovePassword(Path.GetFullPath(FromPath), Password ?? "", Path.GetFullPath(ToPath));
+        }
+
+        public static void RemovePassword(string fromPath, string password, string toPath)
+        {
+            bool tried = false;
+            PdfDocument document = PdfReader.Open(fromPath, PdfDocumentOpenMode.Modify, args =>
+            {
+                if (!tried)
+                {
+                    tried = true;
+                    args.Password = password;
+                }
+                else
+                {
+                    throw new Exception("Password is incorrect.");
+                }
+            });
+
+            if (document != null)
+            {
+                document.SecuritySettings.DocumentSecurityLevel = PdfDocumentSecurityLevel.None;
+                foreach (PdfPage page in document.Pages)
+                {
+                    ;
+                }
+                document.Save(toPath);
+            }
+            else
+            {
+                throw new Exception("Could not open file.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+﻿# PdfDecrypt
+## Requirements
+Windows 7 or later
+
+## Installation
+1. Download the latest release from [Releases](https://github.com/34j/PdfDecrypt/releases).
+2. Add the folder where the program is located to the environment variables.
+
+## Usage
+```console
+> pdfdecrypt from.pdf
+Password: ********
+```
+or
+```console
+> pdfdecrypt from.pdf -p password
+```
+`from.decrypted.pdf` will be generated.
+
+```console
+> pdfdecrypt -h
+Usage: PdfDecrypt [options] <FromPath> <ToPath>
+
+Arguments:
+  FromPath                  The path of the file to remove the password from.
+  ToPath (Optional)         The path to save the decrypted file to.
+
+Options:
+  -p|--password <PASSWORD>  The password to use to decrypt the file.
+  -nc|--noclose             Whether not to close the application after decrypting the file.
+  -?|-h|--help              Show help information.
+```
